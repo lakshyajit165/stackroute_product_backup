278979e83a31fbd86fefd8a9511637cb9a885202
purvish1 <purvish1@in.ibm.com>
2019-09-24 16:04:24 +0530

    User UI

---
+++ user-ui/src/app/course-dialog-component/course-dialog-component.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit , Inject} from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+export interface DialogData {
+  description: string;
+}
+@Component({
+  selector: 'app-course-dialog-component',
+  templateUrl: './course-dialog-component.component.html',
+  styleUrls: ['./course-dialog-component.component.css']
+})
+export class CourseDialogComponentComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<CourseDialogComponentComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+    onNoClick(): void {
+      this.dialogRef.close();
+    }
+  ngOnInit() {
+  }
+}
+
---
+++ user-ui/src/app/speed-dial-fab/speed-dial-fab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-speed-dial-fab',
+  templateUrl: './speed-dial-fab.component.html',
+  styleUrls: ['./speed-dial-fab.component.css']
+})
+export class SpeedDialFabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
