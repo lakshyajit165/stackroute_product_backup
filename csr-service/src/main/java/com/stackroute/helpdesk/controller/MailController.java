49437f33d166d7349514436d568dc192d638f57c
Lakshyajit <lakshyajit165@gmail.com>
2019-09-25 17:30:39 +0530

    added responseentity to mailcontroller

--- csr-service/src/main/java/com/stackroute/helpdesk/controller/MailController.java
+++ csr-service/src/main/java/com/stackroute/helpdesk/controller/MailController.java
@@ -1,10 +1,15 @@
 package com.stackroute.helpdesk.controller;
 
 
+import com.sun.mail.iap.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 @CrossOrigin(origins="http://localhost:4200")
@@ -13,15 +18,24 @@ public class MailController {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    HashMap<String, Object> responseObject;
+
     @PostMapping(path = "/callbackmail")
-    public String sendMsg(@RequestBody String email)
+    public ResponseEntity<HashMap<String, Object>> sendMsg(@RequestBody String email)
     {
         SimpleMailMessage simpleMailMessage=new SimpleMailMessage();
         simpleMailMessage.setTo(email);
         simpleMailMessage.setSubject("HelpDesk optimus callback mail");
-        simpleMailMessage.setText("We will get back to you shortly.");
+        simpleMailMessage.setText("We have received your complaint request. We will get back to you shortly.");
+
 
         javaMailSender.send(simpleMailMessage);
-        return "Mail Success!";
+
+        responseObject = new HashMap<>();
+        responseObject.put("result", "Mail Success");
+        responseObject.put("msg", "Mail sent!");
+        responseObject.put("error", false);
+
+        return new ResponseEntity<>(responseObject, HttpStatus.OK)  ;
     }
 }
